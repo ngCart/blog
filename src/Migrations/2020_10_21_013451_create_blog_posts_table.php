9d02fe9d425c470c901f53e35673630505bdc190
lordfaves <favescsskr@gmail.com>
2020-10-21 08:15:23 +0100

    packages

--- src/Migrations/2020_10_21_013451_create_blog_posts_table.php
+++ src/Migrations/2020_10_21_013451_create_blog_posts_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\BlogCategory;
+use \IFrankSmith\Blogman\Models\BlogCategory;
 use IFrankSmith\Blogman\Models\BlogPermission;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
